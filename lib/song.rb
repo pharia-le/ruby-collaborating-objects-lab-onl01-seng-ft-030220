e35e444d7b77e3a205150b8c971b5df289bd1be1
Pharia Le <pharia.le@gmail.com>
2020-03-17 20:59:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,18 @@ class Song
     @name = name
     self.save
   end
+    
+  def artist_name=(name)
+    if @@all_find(name)
+      self.all_find(name)
+    else
+      artist = self.new(name)
+    end
+  end
+  
+  def all_find(name)
+    @@all.find {|song| song.artist == name }
+  end
   
   def self.new_by_filename(filename)
     artist = filename.split(" - ")[0]
